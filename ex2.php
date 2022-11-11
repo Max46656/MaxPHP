8d071bf854ade78a58e3620f508bfe63ef9ce3fa
Max46656 <maxfromsea36@gmail.com>
2022-11-11 15:05:34 +0800

    no message

--- ex2.php
+++ ex2.php
@@ -12,13 +12,11 @@ $Sum = 0;
 $SumT0 = 0;
 $SumT1 = 0;
 $SumT2 = 0;
-
+$X3 = 0;
 foreach ($A as $k0 => $v) {
     foreach ($A[$k0] as $k1 => $v) {
         foreach ($A[$k0][$k1] as $k2 => $v) {
             foreach ($A[$k0][$k1][$k2] as $k3 => $v) {
-                $X3 = 0;
-                $X3 = 0;
                 if ($k3 % 2 == 0) {
                     $X3 += 1;
                 } else {
@@ -27,11 +25,7 @@ foreach ($A as $k0 => $v) {
             }
             if ($X3 == $k2) {
                 $X2 += 1;
-    }
-            if ($X3 == $k2) {
-                $X2 += 1;
             } else {
-                $X2 = 0    } else {
                 $X2 = 0;
             }
         }
@@ -47,8 +41,7 @@ foreach ($A as $k0 => $v) {
         $X0 = false;
     }
 }
-// 判斷陣列中每一行項目均為偶數。
-if ($X0 = true) {
+if ($X0 == true) {
     foreach ($A as $k0 => $v) {
         foreach ($A[$k0] as $k1 => $v) {
             foreach ($A[$k0][$k1] as $k2 => $v) {
@@ -59,20 +52,18 @@ if ($X0 = true) {
                         echo "+";
                         echo $v;
                         echo "+";
-                        // 當為偶數時以奇偶判斷迴圈，並在奇數前後放置+號
                     }
                     $Sum += $v;
                 }
                 echo "=" . $Sum . "<br>";
                 $SumT0 += $Sum;
                 $Sum = 0;
-                // 列印每一行的計算結果並將該結果加入上一維陣列的小計。
             }
             echo $SumT0 . "<br>";
             $SumT1 += $SumT0;
             $SumT0 = 0;
-            // 列印小計並將該結果加入上一維陣列的小計。
-            if ($k1 == 2) {
+
+            if ($k2 == 2) {
                 echo $SumT1 . "<br>";
             }
         }
@@ -80,5 +71,5 @@ if ($X0 = true) {
         $SumT2 += $SumT1;
         $SumT1 = 0;
     }
+    echo "<br>" . $SumT2 . "<br>";
 }
-echo "<br>" . $SumT2 . "<br>";
